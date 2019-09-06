0584aed788506ca548dadfa711254fa9d30ede40
Johnny Kreitzman <kreitzka@gmail.com>
2019-09-06 16:17:13 -0500

    created initial html and js files and added my starting variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var compChoices = str.toLowerCase["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", 
+"S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var wins = 0;
+var losses = 0;
+var turns = 0;
+var lettersGuessed =[];
